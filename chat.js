d672e56f61c401591ff3c7740afb97dda6882a04
خالد المنتصر <k774468815k@gmail.com>
2025-09-21 16:39:22 +0300

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,8 @@
+`javascript
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.querySelector("header");
+  header.addEventListener("click", function() {
+    alert("مرحباً بك في مركز المعرفة!");
+  });
+});
+`
